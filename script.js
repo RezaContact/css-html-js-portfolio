71e97aa1f53aac1af66f0b1cd606c4ecb1fa1b33
Reza Shiraziyan <60160204+RezaContact@users.noreply.github.com>
2023-12-15 02:02:02 +0000

    New

---
+++ script.js
@@ -0,0 +1,9 @@
+function toggleMenu() {
+    // the line below targets the entire menu-links element on html
+    const menu = document.querySelector(".menu-links")
+    const icon = document.querySelector(".hamburger-icon")
+    
+    // it either toggles it open or closes the toggle class
+    menu.classList.toggle("open")
+    icon.classList.toggle("open")
+}
