4f1457d9042fbf291522800c4b0fa7af196dfd4f
Reza Shiraziyan <60160204+RezaContact@users.noreply.github.com>
2023-12-15 16:46:51 +0000

    new2

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
 function toggleMenu() {
-    // the line below targets the entire menu-links element on html
-    const menu = document.querySelector(".menu-links")
-    const icon = document.querySelector(".hamburger-icon")
-    
-    // it either toggles it open or closes the toggle class
-    menu.classList.toggle("open")
-    icon.classList.toggle("open")
+  // the line below targets the entire menu-links element on html
+  const menu = document.querySelector('.menu-links');
+  const icon = document.querySelector('.hamburger-icon');
+
+  // it either toggles it open or closes the toggle class
+  menu.classList.toggle('open');
+  icon.classList.toggle('open');
 }
+
+function animalStartsWithA(animal) {
+  return animal.toLowerCase().startsWith('a');
+}
+
+console.log(animalStartsWithA('Antelope')); // true
+console.log(animalStartsWithA('Bear')); // false
+
+
+
+const test = 1 2 3 4; 
+const hello = console.log('he llo'   )
+
